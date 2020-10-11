b2bb7263f19e2ae57d2f62993c60a59387d3f4ac
Luisbaduy97 <luisnavarrete1997@gmail.com>
2020-10-11 04:17:49 -0500

    Create file to convert text data to csv format

---
+++ codes/get-routes.py
@@ -0,0 +1,12 @@
+import pandas as pd
+# Reading txt with routes
+f = open('routes.txt', 'r')
+# Save text in data variable
+data = f.read()
+data = data.replace('./', '')
+# print(data)
+# Split text by pattern
+routes_arr = data.split(',')
+routes_f = pd.DataFrame(routes_arr,columns=(['routes']))
+routes_f.to_csv('patients_routes.csv', index=False)
+# print(len(routes_arr))
